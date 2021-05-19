32160b695031b31ba8ebe296f9c85ead2034305e
Bruno Silva <bruno.silva@itella.com>
2021-05-19 10:49:33 +0300

    Added Reddit link

--- frontend/src/components/shared/LorFooter.vue
+++ frontend/src/components/shared/LorFooter.vue
@@ -1,6 +1,12 @@
 <template>
   <footer class="bg-dark text-center text-lg-start">
     <div class="text-center p-3 text-white">
+      Do you have any suggestion/question?
+      <a href="https://www.reddit.com/r/RuneterraVideos" target="_blank" class="text-white">
+        <font-awesome-icon class="mx-1" size="lg" :icon="['fab', 'reddit']" />
+      </a>
+    </div>
+    <div class="text-center p-1 text-white">
       We <span class="mx-1">❤</span> open-source
       <a href="https://github.com/brunosc/lor-gamewise-videos" target="_blank" class="text-white">
         <font-awesome-icon class="mx-1" size="lg" :icon="['fab', 'github']" />
